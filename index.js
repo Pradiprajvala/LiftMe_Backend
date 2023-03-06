f9965ee6bcb483694bff0e76a0493395fdcb35d6
PradipRaj <u20ce053@ced.svnit.ac.in>
2023-03-06 16:23:44 +0530

    allowed credentials in cores

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
 
